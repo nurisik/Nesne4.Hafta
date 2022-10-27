1fe4f39e190ec96bde389234f83c7aad6b09c2e2
ASUS <ASUS@10.48.126.106>
2022-10-27 11:19:54 +0300

    First

--- diziler/src/diziler/deneme2.java
+++ diziler/src/diziler/deneme2.java
@@ -6,16 +6,40 @@ public class deneme2 {
 		//System.out.printf("%s%8s%n","Index","Value");
 		//for(int counter=0; counter < array.length; counter++) {
 			//System.out.printf("%5d%8d%n", counter,array[counter]);
-		int[]array= {32,27,64,18,95,14,90,70,60,37};
+		//int[]array= {32,27,64,18,95,14,90,70,60,37};
+		//System.out.printf("%s%8s%n","Index","Value");
+		//for (int counter = 0; counter < array.length; counter++ ) {
+			//System.out.printf("%5d%8d%n",counter,array[counter]);
+			// for(int i = 0; i < 6; i++)
+		      //  {
+		            final int ARRAY_LENGHT=10;
+			 int[] array =new int [ARRAY_LENGHT];
+			 for (int counter = 0; counter < array.length; counter++) {
+				 array[counter]=2 + 2 *counter;
+				 
+			 }
 		System.out.printf("%s%8s%n","Index","Value");
-		for (int counter = 0; counter < array.length; counter++ ) {
-			System.out.printf("%5d%8d%n",counter,array[counter]);
-		
+		for (int counter = 0; counter< array.length; counter++) {
+			System.out.printf("%5d%8d%n", counter, array[counter]);
 		}
+		 
+		        //}
+			 
+			 //for(int i = array.length+1; i <= 0; i++)
+		       // {
+		         //   System.out.println("Dizinin " + (array.length+i) + ". elemani = " + (array[i]));
+		        //}
+			 
+			 //for(int i = array.length-1; i >= 0; i--)
+		      //  {
+		        //    System.out.println("Dizinin " + (array.length-i) + ". elemani = "+ array[i]);
+		        //}
 			
 		}
 		
-		
 	}
 
-}
+		
+	
+
+
