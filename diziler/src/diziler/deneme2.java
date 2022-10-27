1caa54dba3563f97e5005d5cd294b1dbf5a9f201
ASUS <ASUS@10.48.126.106>
2022-10-27 10:18:13 +0300

    first

--- diziler/src/diziler/deneme2.java
+++ diziler/src/diziler/deneme2.java
@@ -1,6 +1,21 @@
 package diziler;
 
 public class deneme2 {
-	public static void main(String[] args)
+	public static void main(String[] args) {
+		//int[] array=new int[10];
+		//System.out.printf("%s%8s%n","Index","Value");
+		//for(int counter=0; counter < array.length; counter++) {
+			//System.out.printf("%5d%8d%n", counter,array[counter]);
+		int[]array= {32,27,64,18,95,14,90,70,60,37};
+		System.out.printf("%s%8s%n","Index","Value");
+		for (int counter = 0; counter < array.length; counter++ ) {
+			System.out.printf("%5d%8d%n",counter,array[counter]);
+		
+		}
+			
+		}
+		
+		
+	}
 
 }
